81cca9c72c5f3b014fad5a46a14aa24a7a812cd6
Jason Cameron <dev.jasoncameron@gmail.com>
2023-06-14 20:05:06 -0500

    section 5, vars and coments

--- Udemy/CppCourse/Udemy/Section05/main.cpp
+++ Udemy/CppCourse/Udemy/Section05/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 
+using namespace std;    // Use the entire std namespace
+                    // use ^g to get multiple selections of the same highlighted
+
+
+// Getting rid of std
+/*
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int favorite_number;
+
+    cout << "Enter your favorite number between 1 and 100: ";
+
+    cin >> favorite_number;
+
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!!, " << favorite_number << " is my favorite number!" << endl;
+    return 0;
+}
+*/
+
+// cin and cout
+int main(){
+
+//    cout << "Hello world" << endl;
+
+//    cout << "Hello";
+//    cout << "World" << endl;
+
+//    cout << "Hello world!" << endl;
+//    cout << "Hello" << " world!" << endl;
+//    cout << "Hello " << "world!\n";
+//    cout << "Hello\nOut\nThere\n";
+
+    int num1;
+    int num2;
+    double num3;
+
+//    cout << "Enter an integer: ";
+//    cin >> num1;
+//    cout << "You entered in " << num1 << endl;
+
+//    cout << "Enter a first integer: ";
+//    cin >> num1;
+//    cout << "Enter a second integer: ";
+//    cin >> num2;
+//    cout << "You entered " << num1 << " and " << num2 << endl;
+
+
     return 0;
 }
