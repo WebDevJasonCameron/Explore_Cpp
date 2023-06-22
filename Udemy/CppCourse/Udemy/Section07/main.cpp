8e58205296a02fb2555638d007f7be799b640045
Jason Cameron <dev.jasoncameron@gmail.com>
2023-06-22 06:07:03 -0500

    section 7 challenge comp

--- Udemy/CppCourse/Udemy/Section07/main.cpp
+++ Udemy/CppCourse/Udemy/Section07/main.cpp
@@ -50,6 +50,7 @@ int main() {
 */
 
 // VECTORS
+/*
 int main() {
 
     //DiF ways to initialize a vector
@@ -112,7 +113,66 @@ int main() {
 
 //    cout << "This should cause an exception!!" << test_scores.at(10);
 
+    return 0;
+}
+*/
 
+// Challenge
+/*
+ * Write a C++ program as follows:
+ *
+ * Declare 2 empty vectors of integers named
+ * vector1 and vector2
+ *
+ * Add 10 and 20 to vector1 dynamically using push_back
+ * Display the elements in vector1 using the at() method as well as its size using the size() method
+ *
+ * Add t00 and 200 to vector2 dynamically using push_back
+ * Display the elements in vector2 using the at() method as well as its size using the size() method
+ *
+ * Declare an empty 2D vector called vector_2d
+ * Remember that a 2D vector is a vector of vector<int>
+ *
+ * Add vector2 to vector_2d dynamically using push_back
+ * Add vector to vector_2d dynamically using push_back
+ *
+ * Display the elements in vector_2d using the at() method
+ *
+ * Change vector1.at(0) to 1000
+ *
+ * Display the element in vector_2d again using the at() method
+ *
+ * Display the elements in vector1
+ *
+ * What did you expect?  Did you get what you expected?  What do you think happened?
+ */
+int main(){
+
+    vector <int> vector1;
+    vector <int> vector2;
+
+    vector1.push_back(10);
+    vector1.push_back(20);
+    cout << "vector 1 at 0 place is: " << vector1.at(0) << endl;
+    cout << "vector 1 at 1 place is: " << vector1.at(1) << endl;
+
+    vector2.push_back(100);
+    vector2.push_back(200);
+    cout << "vector 2 at 0 place is: " << vector2.at(0) << endl;
+    cout << "vector 2 at 1 place is: " << vector2.at(1) << endl;
+
+    vector <vector<int>> vector_2d;
+
+    vector_2d.push_back(vector1);
+    vector_2d.push_back(vector2);
+    cout << "vector 2d at 0 0 place is: " << vector_2d.at(0).at(0) << endl;
+    cout << "vector 2d at 0 1 place is: " << vector_2d.at(0).at(1) << endl;
+    cout << "vector 2d at 1 0 place is: " << vector_2d.at(1).at(0) << endl;
+    cout << "vector 2d at 1 1 place is: " << vector_2d.at(1).at(1) << endl;
+
+    vector1.at(0) = 1000;
+    cout << "vector 1 at 0 place changed to: " << vector1.at(0) << endl;
+    cout << "vector 2d at 0 0 place changed to: " << vector_2d.at(0).at(0) << endl;
 
 
     return 0;
