c99f6a930865a105843a195524ed64a6848c0711
Jason Cameron <dev.jasoncameron@gmail.com>
2023-06-23 06:15:48 -0500

    operands lsn

--- Udemy/CppCourse/Udemy/Section08/main.cpp
+++ Udemy/CppCourse/Udemy/Section08/main.cpp
@@ -4,10 +4,90 @@ using namespace std;
 
 // EXPRESSIONS AND STATEMENTS
 
-//
+// Assignment Operator
+/*
 int main() {
 
+    // initialization
+    int num1 {10};
+    int num2 {20};
 
+    // assignment
+//    num1 = 100;
+//    num1 = num2;
+    num1 = num2 = 1000;
+
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+    return 0;
+}
+*/
+
+// Operands
+/*
+int main(){
+
+    int num1 {200};
+    int num2 {100};
+
+//    cout << num1  << " + " << num2 << " = " << num1 + num2 << endl;
+
+    // add
+//    int result {0};
+//    result = num1 + num2;
+//    cout << num1  << " + " << num2 << " = " << result << endl;
+
+    // subtract
+//    int result {0};
+//    result = num1 - num2;
+//    cout << num1  << " - " << num2 << " = " << result << endl;
+
+    // multiply
+//    int result {0};
+//    result = num1 * num2;
+//    cout << num1  << " * " << num2 << " = " << result << endl;
+
+    // divide
+//    int result {0};
+//    result = num2 / num1;
+//    cout << num2  << " / " << num1 << " = " << result << endl;
+
+    // modulo
+//    int result {0};
+//    result = num1 % num2;
+//    cout << num1  << " % " << num2 << " = " << result << endl;
+
+//    num1 = 10;
+//    num2 = 3;
+//
+//    int result {0};
+//    result = num1 % num2;
+//    cout << num1  << " % " << num2 << " = " << result << endl;
+
+    // Following PEMDAS
+    int result = num1 * 10 + num2;
 
     return 0;
 }
+*/
+
+// Problem
+int main(){
+
+    cout << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
