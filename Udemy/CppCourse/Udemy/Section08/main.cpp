82250cad00144253cbc09c3e33ba8bbe20a08b38
Jason Cameron <dev.jasoncameron@gmail.com>
2023-06-24 09:20:58 -0500

    euro to dollar prob comp

--- Udemy/CppCourse/Udemy/Section08/main.cpp
+++ Udemy/CppCourse/Udemy/Section08/main.cpp
@@ -71,11 +71,23 @@ int main(){
 }
 */
 
-// Problem
+// Problem: Convert euro to dollars
 int main(){
 
-    cout << endl;
+    const double usd_per_euro{1.19};
+    double euros {0.0};
+    double dollars {0.0};
 
+    cout << "Welcom to the EUR to USD converter" << endl;
+    cout << "Enter the value in EUR: ";
+
+    cin >> euros;
+    dollars = euros * usd_per_euro;
+    cout << euros << " euros is equivalent to " << dollars << " dollars" << endl;
+
+
+
+    cout << endl;
     return 0;
 }
 
