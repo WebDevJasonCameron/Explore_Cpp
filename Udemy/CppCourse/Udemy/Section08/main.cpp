cda3b3dc8e8cace2a6a4d1bc2b4f0fc0fd6a3c87
Jason Cameron <dev.jasoncameron@gmail.com>
2023-06-25 18:21:47 -0500

    comp mix type expressions

--- Udemy/CppCourse/Udemy/Section08/main.cpp
+++ Udemy/CppCourse/Udemy/Section08/main.cpp
@@ -105,6 +105,7 @@ int main(){
  * Don't over use!
  * Alert!!! Never use it twice for the same var in the same statement
  */
+/*
 int main(){
 
     int counter {10};
@@ -170,7 +171,41 @@ int main(){
 
     return 0;
 }
+*/
 
+// Mixed Type Expressions
+/*
+ * Ask the user to enter 3 integers
+ * Calculate the sum of the integers then
+ * calculate the average of the 3 integers
+ *
+ * Display the 3 integers entered
+ * the sum of the 3 integers and
+ * the average of the 3 integers
+ */
+int main(){
+
+    int total {};
+    int num1 {}, num2 {}, num3 {};
+    const int count {3};
+
+    cout << "Enter 3 integers separated by spaces: ";
+    cin >> num1 >> num2 >> num3;
+
+    total = num1 + num2 + num3;
+
+    double average {0.0};
+
+    average = static_cast<double>(total) / count;
+
+    cout << "The 3 numbers were: " << num1 << ", " << num2 << ", " << num3 << endl;
+    cout << "The sum of the number is: " << total << endl;
+    cout << "The average of the numbers is: " << average << endl;
+
+
+    cout << endl;
+    return 0;
+}
 
 
 
