ca2b432769d92f1b1e182a9faca084c99de28ffc
Jason Cameron <dev.jasoncameron@gmail.com>
2023-06-26 06:20:35 -0500

    lsn comp

--- Udemy/CppCourse/Udemy/Section08/main.cpp
+++ Udemy/CppCourse/Udemy/Section08/main.cpp
@@ -212,6 +212,45 @@ int main(){
 // Testing for Equality
 int main(){
 
+    bool equal_result {false};
+    bool not_equal_result {false};
+
+    int num1 {}, num2 {};
+
+     cout << boolalpha;       // will display true / false instead of 1/0 for booleans
+
+//    cout << "Enter two integers seperated by a space: ";
+//    cin >> num1 >> num2;
+//    equal_result = { num1 == num2 };
+//    not_equal_result = {num1 != num2 };
+//    cout << "Comparison result (equals): " << equal_result << endl;
+//    cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+    char
+//    cout << "Enter two characters separated by a space: ";
+//    cin >> char1 >> char2;
+//    equal_result = { char1 == char2 };
+//    not_equal_result = { char1 != char2 };
+//    cout << "Comparison result (equals): " << equal_result << endl;
+//    cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+    char double1 {}, double2 {};
+//    cout << "Enter two characters separated by a space: ";
+//    cin >> double1 >> double2;
+//    equal_result = { double1 == double2 };
+//    not_equal_result = { double1 != double2 };
+//    cout << "Comparison result (equals): " << equal_result << endl;
+//    cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+
+    cout << "Enter integer and double separated by a space: ";
+    cin >> num1 >> double1;
+    equal_result = { num1 == double1 };
+    not_equal_result = { num1 != double1 };
+    cout << "Comparison result (equals): " << equal_result << endl;
+    cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+
 
     return 0;
 }
