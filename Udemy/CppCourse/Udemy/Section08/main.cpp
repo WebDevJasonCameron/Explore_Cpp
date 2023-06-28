19f1ea465d2630126cba4df7a78764533bbe430f
Jason Cameron <dev.jasoncameron@gmail.com>
2023-06-28 06:07:13 -0500

    Logical Operators lsn comp

--- Udemy/CppCourse/Udemy/Section08/main.cpp
+++ Udemy/CppCourse/Udemy/Section08/main.cpp
@@ -183,6 +183,7 @@ int main(){
  * the sum of the 3 integers and
  * the average of the 3 integers
  */
+
 /*
 int main(){
 
@@ -256,6 +257,7 @@ int main(){
 */
 
 // Relational Operators
+/*
 int main(){
 
     int num1 {}, num2 {};
@@ -283,7 +285,74 @@ int main(){
 
     return 0;
 }
+*/
 
+// Logical Operators
+int main() {
+
+    int num{};
+    const int lower {10};
+    const int upper {20};
+
+    cout << boolalpha;
+
+    // Determine if an entered integer is between two other integers
+    // assuming lower < upper
+//    cout <<"Enter an integer - the bounds are " << lower << " and " << upper << ": ";
+//    cin >> num;
+//
+//    bool within_bounds {false};
+//
+//    within_bounds = {num > lower && num < upper};
+//    cout << num << " is between " << lower << " and " << upper << ": " << within_bounds << endl;
+
+    // Determine if an entered integer is outside two other integers
+    // assuming lower < upper
+//    cout <<"Enter an integer - the bounds are " << lower << " and " << upper << ": ";
+//    cin >> num;
+//
+//    bool outside_bounds {false};
+//
+//    outside_bounds = {num < lower || num > upper};
+//    cout << num << " is outside " << lower << " and " << upper << ": " << outside_bounds << endl;
+
+    // Determine if an entered integer is exactly on the boundary
+    // assuming lower < upper
+//    cout <<"Enter an integer - the bounds are " << lower << " and " << upper << ": ";
+//    cin >> num;
+//
+//    bool on_bounds {false};
+//
+//    on_bounds = {num == lower || num == upper};
+//    cout << num << " is on one of the bounds which are  " << lower << " and " << upper << ": " << on_bounds << endl;
+
+
+    // Determine if you need to wear a coat based on temperature and wind speed
+    bool wear_coat {false};
+    double temperature {};
+    int wind_speed {};
+
+    const int wind_speed_for_coat {25};
+    const double temperature_for_coat {45};
+
+    // Require a coat if either wind is too high or temperature is too low
+    cout << "\nEnter the current temperature:   ";
+    cin >> temperature;
+
+    cout << "Enter wind speed in (MPH):  ";
+    cin >> wind_speed;
+
+    wear_coat = (temperature < temperature_for_coat || wind_speed > wind_speed_for_coat);
+    cout << "Do you need to wear a coat using OR?  " << wear_coat << endl;
+
+    // Require a coat if BOTH the wind speed is too high AND temperature is too low
+    wear_coat = (temperature < temperature_for_coat && wind_speed > wind_speed_for_coat);
+    cout << "Do you need to wear a coat using AND?  " << wear_coat << endl;
+
+
+    cout << endl;
+    return 0;
+}
 
 
 
