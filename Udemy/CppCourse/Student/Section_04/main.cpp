9d134587edfb5c3c4666ba2c4e5e7a36bb04bbda
Jason Cameron <dev.jasoncameron@gmail.com>
2024-12-25 04:50:48 -0600

    section 4 nearly cmplt

--- Udemy/CppCourse/Student/Section_04/main.cpp
+++ Udemy/CppCourse/Student/Section_04/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int favorite_number;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number tool!" << std::endl;
+
     return 0;
 }
