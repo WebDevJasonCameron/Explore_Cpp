c7375f421779cc972e447bdca5928d1ec01fbf03
Jason Cameron <dev.jasoncameron@gmail.com>
2024-12-31 18:48:02 -0600

    expressions and statements

--- Udemy/CppCourse/Student/Section_08/main.cpp
+++ Udemy/CppCourse/Student/Section_08/main.cpp
@@ -4,7 +4,24 @@ using namespace std;
 
 int main() {
 
+    int num1 {200};
+    int num2 {100};
 
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+
+    int result {0};
+
+    result = num1 + num2;
+    cout << num1 << " + " << num2 << " = " << result << endl;
+
+    result = num1 - num2;
+    cout << num1 << " - " << num2 << " = " << result << endl;
+
+    result = num1 * num2;
+    cout << num1 << " * " << num2 << " = " << result << endl;
+
+    result = num1 / num2;
+    cout << num1 << " / " << num2 << " = " << result << endl;
 
     return 0;
 }
