8afbf17b93e3180787175bad73a1be7d5992bac4
Jason Cameron <dev.jasoncameron@gmail.com>
2025-01-02 18:10:33 -0600

    cont section 8

--- Udemy/CppCourse/Student/Section_08/main.cpp
+++ Udemy/CppCourse/Student/Section_08/main.cpp
@@ -4,26 +4,81 @@ using namespace std;
 
 int main() {
 
-    /*
-     * EUR
+    /*=======================================================
+     * INCREMENT AND DECREMENT OPERATOR
+
+        Increment Operator ++
+        Decrement Operator --
+
+        Increments or decrements its operand by 1
+        Can be used with integers, floating point types and pointers
+
+        Prefix ++num
+        Postfix num--
+
+        Don't overuse this operator!
+        ALERT!!!Never use it twice for the same variable in the same statement!
+
      */
-    const double usdPerEUR {1.19};
 
-    cout << "Welcome to the EUR to USD converter" << endl;
-    cout << "Enter the value in EUR: ";
+    int counter {10};
+    int result {0};
 
-    double euros (0.0);
-    double dollars {0.0};;
+    // Example 1 - simple increment
+//    cout << "Counter: " << counter << endl;
+//
+//    counter = counter + 1;
+//    cout << "Counter: " << counter << endl;
+//
+//    counter++;
+//    cout << "Counter: " << counter << endl;
+//
+//    ++counter;
+//    cout << "Counter: " << counter << endl;
 
-    cin >> euros;
-    dollars = euros * usdPerEUR;
+    // Example 2 - preincrement
+//    counter = 10;
+//    result = 0;
+//
+//    cout << "Counter: " << counter << endl;
+//
+//    result = ++counter;                         // Note the pre increment
+//    cout << "Counter: " << counter << endl;
+//    cout << "Result: " << result << endl;
 
-    cout << euros << "euros is equivalent to " << dollars << " dollars" << endl;
+    // Example 3 post increment
+    counter = 10;
+    result= 0;
 
-    cout << endl;
+    cout << "Count: " << counter << endl;
+
+    result = counter++;                         // Note the post increment
+    cout << "Counter: " << counter << endl;
+    cout << "Result: " << result << endl;
+
+
+    // Example 4
 
 
 
+
+    /*=======================================================
+     * EUR CONVERTER
+     */
+//    const double usdPerEUR {1.19};
+//
+//    cout << "Welcome to the EUR to USD converter" << endl;
+//    cout << "Enter the value in EUR: ";
+//
+//    double euros (0.0);
+//    double dollars {0.0};;
+//
+//    cin >> euros;
+//    dollars = euros * usdPerEUR;
+//
+//    cout << euros << "euros is equivalent to " << dollars << " dollars" << endl;
+//
+//    cout << endl;
 
 
 
