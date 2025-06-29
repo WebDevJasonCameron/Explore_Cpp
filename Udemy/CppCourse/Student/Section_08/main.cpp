2f6cf303b506b8d6f59a6356349f7c7ec711d030
Jason Cameron <dev.jasoncameron@gmail.com>
2025-06-28 20:35:17 -0500

    cont learning, now in section 9

--- Udemy/CppCourse/Student/Section_08/main.cpp
+++ Udemy/CppCourse/Student/Section_08/main.cpp
@@ -4,25 +4,101 @@ using namespace std;
 
 int main() {
 
+    int num {};
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num < 10) {
+        cout << num << " is less than ten" << endl;
+    }
+    else {
+        cout << num << " is equal or greater than ten" << endl;
+    }
+
+    if (num > 10) {
+        cout << num << " is greater than ten" << endl;
+    }
+    else {
+        cout << num << " is less than or equal to ten" << endl;
+    }
+
+    /*
+    // define conversion value in cents
+    const int dollar_value {100};
+    const int quarter_value {25};
+    const int dime_value {10};
+    const int nickel_value {5};
+    const int penny_value {1};
+
+    int change_amount {};
+
+    // Solution 1 - not using the modulo operator
+
+    cout << "Enter an amount in cents: ";
+    cin >> change_amount;
+
+    int balance {}, dollars{}, quarters{}, dimes{}, nickels{}, pennies{};
+
+    dollars = change_amount / dollar_value;
+    balance = change_amount - (dollars * dollar_value);
+
+    quarters = balance / quarter_value;
+    balance = balance - (quarters * quarter_value);
+
+    dimes = balance / dime_value;
+    balance = balance - (dimes * dime_value);
+
+    nickels = balance / nickel_value;
+    balance = balance - (nickels * nickel_value);
+
+    pennies = balance / penny_value;
+
+    cout << "That will be " << dollars << " dollars, " << quarters << " quarters, " << dimes << " dimes, " << nickels << " nickels, and " << pennies << " pennies." << endl;
+    */
+
+
+
     /*=======================================================
-     * INCREMENT AND DECREMENT OPERATOR
+    *
+    int total {};
+    int num1 {}, num2 {}, num3 {};
+    const int count{3};
 
-        Increment Operator ++
-        Decrement Operator --
+    cout << "Enter 3 integers separated by spaces: ";
+    cin >> num1 >> num2 >> num3;
 
-        Increments or decrements its operand by 1
-        Can be used with integers, floating point types and pointers
+    total = num1 + num2 + num3;
 
-        Prefix ++num
-        Postfix num--
+    double average {0.0};
 
-        Don't overuse this operator!
-        ALERT!!!Never use it twice for the same variable in the same statement!
+    average = static_cast<double>(total) / count;
 
-     */
+    cout << "The 3 numbers were: " << num1 << " " << num2 << " " << num3 << endl;
+    cout << "The sum of the numbers is " << total << endl;
+    cout << "The average of the number is " << average << endl;
 
-    int counter {10};
-    int result {0};
+cout << endl;
+*/
+
+/*=======================================================
+ * INCREMENT AND DECREMENT OPERATOR
+
+    Increment Operator ++
+    Decrement Operator --
+
+    Increments or decrements its operand by 1
+    Can be used with integers, floating point types and pointers
+
+    Prefix ++num
+    Postfix num--
+
+    Don't overuse this operator!
+    ALERT!!!Never use it twice for the same variable in the same statement!
+
+ */
+
+//    int counter {10};
+//    int result {0};
 
     // Example 1 - simple increment
 //    cout << "Counter: " << counter << endl;
@@ -69,15 +145,15 @@ int main() {
 //    cout << "Result: " << result << endl;
 
     // Example 5
-    counter = 10;
-    result= 0;
-
-    cout << "Counter: " << counter << endl;
-
-    result = counter++ + 10;
-
-    cout << "Counter: " << counter << endl;
-    cout << "Result: " << result << endl;
+//    counter = 10;
+//    result= 0;
+//
+//    cout << "Counter: " << counter << endl;
+//
+//    result = counter++ + 10;
+//
+//    cout << "Counter: " << counter << endl;
+//    cout << "Result: " << result << endl;
 
 // Wat?
 // Quick Git test...
@@ -147,7 +223,11 @@ int main() {
 //
 //    cout << 5.0/10.0 << endl;
 
-    cout << endl;
+ //   cout << endl;
+
+
+
+
 
     return 0;
 }
